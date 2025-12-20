812654b6cd02b0c79365c32bae7a5397e3e15718
Omar abu Shanab <omarabushanab03@gmail.com>
2025-12-20 23:44:01 +0200

    mafeewsh message

--- app/UI_to_model.py
+++ app/UI_to_model.py
@@ -1,14 +1,20 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 import torch.nn.functional as F
+from pathlib import Path
 
-model_path = "xlm-r-phishing-final"   # path relative to your script
-
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = AutoModelForSequenceClassification.from_pretrained(model_path)
+model_path = (
+    Path(__file__).resolve()
+    .parents[1]          # fraud-detection/
+    / "xlm-r-phishing-final"
+)
+model_path = str(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
+model = AutoModelForSequenceClassification.from_pretrained(model_path, local_files_only=True)
 model.eval()
 
 def classify_text(text):
+    
     inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
 
     with torch.no_grad():
